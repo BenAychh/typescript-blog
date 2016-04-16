e921b78469347840feb11d56dcf6c344cf62f07c
BenAychh <ben.tomas.hernandez@gmail.com>
2016-04-16 15:47:13 -0600

    S3 uploading

--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -1,3 +1,23 @@
 var typeBinder = new Binder();
 typeBinder.addListener(new Bound('inputText'));
 typeBinder.addListener(new ParsedBound('inputResult'));
+let uploader = new S3upLoader('file', '/posts/signS3', (url) => {
+    insertInputTextAtCaret('<img class="img-responsive" src="' + url + '" alt="">');
+});
+function insertInputTextAtCaret(text) {
+    text = text;
+    let inputText = document.getElementById('inputText');
+    if (inputText.selectionStart || inputText.selectionStart === 0) {
+        var startPos = inputText.selectionStart;
+        var endPos = inputText.selectionEnd;
+        inputText.value = inputText.value.substring(0, startPos) +
+            text +
+            inputText.value.substring(endPos, inputText.value.length);
+        inputText.selectionStart = startPos + text.length;
+        inputText.selectionEnd = startPos + text.length;
+    }
+    else {
+        inputText.value += text;
+    }
+}
+;
--- public/javascripts/master.ts
+++ public/javascripts/master.ts
@@ -1,4 +1,24 @@
 /// <reference path="bind.ts"/>
+/// <reference path="s3FileUpload.ts"/>
 var typeBinder = new Binder();
 typeBinder.addListener(new Bound('inputText'));
 typeBinder.addListener(new ParsedBound('inputResult'));
+let uploader = new S3upLoader('file', '/posts/signS3', (url) => {
+  insertInputTextAtCaret('<img class="img-responsive" src="' + url + '" alt="">');
+});
+function insertInputTextAtCaret (text) {
+  text = text;
+  let inputText = <HTMLInputElement>document.getElementById('inputText');
+  if (inputText.selectionStart || inputText.selectionStart === 0) {
+    // Others
+    var startPos = inputText.selectionStart;
+    var endPos = inputText.selectionEnd;
+    inputText.value = inputText.value.substring(0, startPos) +
+      text +
+      inputText.value.substring(endPos, inputText.value.length);
+    inputText.selectionStart = startPos + text.length;
+    inputText.selectionEnd = startPos + text.length;
+  } else {
+    inputText.value += text;
+  }
+};
