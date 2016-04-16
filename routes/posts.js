522be0c6e09c12c2275cd1806dfb78f6f10c1999
BenAychh <ben.tomas.hernandez@gmail.com>
2016-04-16 15:46:46 -0600

    Added image uploading to S3

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 var helpers = require('../auth/helpers.js');
+var aws = require('aws-sdk');
+var md5 = require('js-md5');
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
 router.get('/', function (req, res, next) {
   models.blogposts.findAll({
@@ -18,8 +23,34 @@ router.get('/', function (req, res, next) {
   });
 });
 
+router.get('/signS3', (req, res) => {
+  aws.config.update({ accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY });
+  var s3 = new aws.S3();
+  var fileName = new Date().getTime()
+    + req.query.fileName.substring(req.query.fileName.lastIndexOf('.'));
+  var s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: req.query.fileType,
+    ACL: 'public-read',
+  };
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+
+      var returnData = {
+        signedRequest: data,
+        url: 'https://' + S3_BUCKET + '.s3.amazonaws.com/' + fileName,
+      };
+      res.json(returnData);
+    }
+  });
+});
+
 router.get('/create', helpers.ensureAuthor, function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('createblog', { title: 'Express' });
 });
 
 router.get('/show', (req, res, next) => {
